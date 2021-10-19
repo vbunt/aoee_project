77e4d20627e9d697d054fcbf0492ef02773fde57
vbunt <54185665+vbunt@users.noreply.github.com>
2021-10-19 11:14:09 +0300

    Update project_web.py

--- myproject/project_web.py
+++ myproject/project_web.py
@@ -1,209 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
-
-import sqlite3
+from math import ceil
+import slow_search
 import re
-from natasha import (
-    Segmenter,
-    MorphVocab,
-
-    NewsEmbedding,
-    NewsMorphTagger,
-    Doc
-)
-segmenter = Segmenter()
-morph_vocab = MorphVocab()
-emb = NewsEmbedding()
-morph_tagger = NewsMorphTagger(emb)
-
-conn = sqlite3.connect('C:/Users/anuta/myproject/Harry_Potter80_filled.db', check_same_thread=False)
-cur = conn.cursor()
-
-
-posser = ['NUM', 'X', 'ADJ', 'SYM', 'ADV', 'AUX', 'ADP', 'PROPN', 'NOUN',
-          'SCONJ', 'CCONJ', 'DET', 'PRON', 'PART', 'VERB', 'INTJ']
-
-exact_form_sql = ('''
-    select tokens.word, tokens.token_id, sentences.sentence, sentences.sentence_id, texts.title, texts.source
-    from main
-    join tokens on tokens.token_id = main.token_id
-    join sentences on sentences.sentence_id = main.sentence_id
-    where tokens.word = ?''')
-
-pos_sql = ('''
-    select tokens.word, tokens.token_id, sentences.sentence, sentences.sentence_id
-    from main
-    join tokens on tokens.token_id = main.token_id
-    join sentences on sentences.sentence_id = main.sentence_id
-    where tokens.pos = ?''')
-
-word_pos_sql = ('''
-    select tokens.word, tokens.token_id, sentences.sentence, sentences.sentence_id
-    from main
-    join tokens on tokens.token_id = main.token_id
-    join sentences on sentences.sentence_id = main.sentence_id
-    where tokens.lemma = ? and tokens.pos = ?''')
-
-lemma_sql = ('''
-    select tokens.word, tokens.token_id, sentences.sentence, sentences.sentence_id
-    from main
-    join tokens on tokens.token_id = main.token_id
-    join sentences on sentences.sentence_id = main.sentence_id
-    where tokens.lemma = ?''')
-
-om_exact_form_sql = ('''
-    select tokens.word, tokens.token_id, sentences.sentence, sentences.sentence_id
-    from main
-    join tokens on tokens.token_id = main.token_id
-    join sentences on sentences.sentence_id = main.sentence_id
-    where tokens.word = ? and tokens.token_id == ? and sentences.sentence_id = ? ''')
-
-om_pos_sql = ('''
-    select tokens.word, tokens.token_id, sentences.sentence, sentences.sentence_id
-    from main
-    join tokens on tokens.token_id = main.token_id
-    join sentences on sentences.sentence_id = main.sentence_id
-    where tokens.pos = ? and tokens.token_id == ? and sentences.sentence_id = ?''')
-
-om_word_pos_sql = ('''
-    select tokens.word, tokens.token_id, sentences.sentence, sentences.sentence_id
-    from main
-    join tokens on tokens.token_id = main.token_id
-    join sentences on sentences.sentence_id = main.sentence_id
-    where tokens.lemma = ? and tokens.pos = ? and tokens.token_id == ? and sentences.sentence_id = ?''')
-
-om_lemma_sql = ('''
-    select tokens.word, tokens.token_id, sentences.sentence, sentences.sentence_id
-    from main
-    join tokens on tokens.token_id = main.token_id
-    join sentences on sentences.sentence_id = main.sentence_id
-    where tokens.lemma = ? and tokens.token_id == ? and sentences.sentence_id = ?''')
-
-text_sql = ('''
-    select texts.source, texts.title
-    from sentences
-    join texts on texts.text_id = sentences.text_id
-    where sentence = ?''')
-
-
-def one_item(item):
-    if '"' in item:
-        exact_form = re.search('[а-яА-ЯёЁ]+', item)[0]
-        cur.execute(exact_form_sql, [exact_form])
-        data = cur.fetchall()
-
-    elif item in posser:
-        cur.execute(pos_sql, [item])
-        data = cur.fetchall()
-
-    elif '+' in item:
-        word = re.search('[а-яА-ЯёЁ]+', item)[0]
-        pos = re.search('[A-Z]+', item)[0]
-        doc = Doc(word)
-        doc.segment(segmenter)
-        doc.tag_morph(morph_tagger)
-        for token in doc.tokens:
-            token.lemmatize(morph_vocab)
-            lemma = token.lemma
-            cur.execute(word_pos_sql, [lemma, pos])
-        data = cur.fetchall()
-
-    else:
-        doc = Doc(item)
-        doc.segment(segmenter)
-        doc.tag_morph(morph_tagger)
-        for token in doc.tokens:
-            token.lemmatize(morph_vocab)
-            lemma = token.lemma
-            cur.execute(lemma_sql, [lemma])
-        data = cur.fetchall()
-
-    return data
-
-
-def one_more_item(item, token_id, sentence_id):
-    if '"' in item:
-        exact_form = re.search('[а-яА-ЯёЁ]+', item)[0]
-        cur.execute(om_exact_form_sql, [exact_form, (token_id + 1), sentence_id])
-        data = cur.fetchall()
-
-    elif item in posser:
-        cur.execute(om_pos_sql, [item, (token_id + 1), sentence_id])
-        data = cur.fetchall()
-
-    elif '+' in item:
-        word = re.search('[а-яА-ЯёЁ]+', item)[0]
-        pos = re.search('[A-Z]+', item)[0]
-        doc = Doc(word)
-        doc.segment(segmenter)
-        doc.tag_morph(morph_tagger)
-        for token in doc.tokens:
-            token.lemmatize(morph_vocab)
-            lemma = token.lemma
-            cur.execute(om_word_pos_sql, [lemma, pos, (token_id + 1), sentence_id])
-        data = cur.fetchall()
-
-    else:
-        doc = Doc(item)
-        doc.segment(segmenter)
-        doc.tag_morph(morph_tagger)
-        for token in doc.tokens:
-            token.lemmatize(morph_vocab)
-            lemma = token.lemma
-            cur.execute(om_lemma_sql, [lemma, (token_id + 1), sentence_id])
-        data = cur.fetchall()
-
-    return data
-
-
-def searcher(request_items):
-    if len(request_items) == 1:
-        issue = []
-        for entry in one_item(request_items[0]):
-            cur.execute(text_sql, [entry[2]])
-            source = cur.fetchall()
-            new = (entry[0], entry[2], source)
-            issue.append(new)
-
-    elif len(request_items) == 2:
-        issue = []
-        first = request_items[0]
-        second = request_items[1]
-        first_answ = one_item(first)
-        for entry in first_answ:
-            good_entry = one_more_item(second, entry[1], entry[3])
-            if len(good_entry) != 0:
-                cur.execute(text_sql, [good_entry[0][2]])
-                source = cur.fetchall()
-                new = (entry[0] + ' ' + good_entry[0][0], good_entry[0][2], source)
-                issue.append(new)
-
-    else:
-        issue = []
-        first = request_items[0]
-        second = request_items[1]
-        third = request_items[2]
-        first_answ = one_item(first)
-        for entry in first_answ:
-            second_answ = []
-            almost_good_entry = one_more_item(second, entry[1], entry[3])
-            if len(almost_good_entry) != 0:
-                second_answ.append(almost_good_entry)
-                for ag_entry in second_answ:
-                    good_entry = one_more_item(third, ag_entry[0][1], ag_entry[0][3])
-                    if len(good_entry) != 0:
-                        cur.execute(text_sql, [good_entry[0][2]])
-                        source = cur.fetchall()
-                        new = (entry[0] + ' ' + ag_entry[0][0] + ' ' + good_entry[0][0], good_entry[0][2], source)
-                        issue.append(new)
-    return issue, len(issue)
-
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'avadakedavra'
-
+form = ''
+n_pages = 0
+result = []
+req = ''
 
 class RequestForm(FlaskForm):
     request = StringField('', validators=[DataRequired()])
@@ -212,17 +20,26 @@ class RequestForm(FlaskForm):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    global form, result, n_pages, req
     form = RequestForm()
     if form.validate_on_submit():
-        request = form.request.data
-        request_items = re.split(' ', request)
-        result = searcher(request_items)
+        req = form.request.data
+        request_items = re.split(' ', req)
+        result = slow_search.searcher(request_items)
+        n_pages = ceil(result[1] / 20)
+        ind = 0
         form.request.data = ''
-        return render_template('result.html', request=request, form=form, result=result)
+        return render_template('result.html', req=req, form=form, result=result, n_pages=n_pages, ind=ind)
     else:
-        request = None
+        req = None
         result = []
-        return render_template('main.html', request=request, form=form, result=result)
+        return render_template('main.html', req=req, form=form, result=result)
+
+
+@app.route('/result')
+def pages():
+    page_no = request.args.get('page')
+    return render_template('result.html', req=req, form=form, result=result, n_pages=n_pages, ind=int(page_no)-1)
 
 
 @app.errorhandler(404)
